ae886d4d30962b5a4e29b99018a72dfedd00097a
karthikeyan.V <karthikarthi85544@gmail.com>
2024-06-01 22:50:17 +0530

    continue interview question java program

---
+++ src/Continue_InterviewQuestion.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Continue_InterviewQuestion {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 10) {
+
+            System.out.println("*");
+            if (i % 2 == 0) {
+                //break;
+               continue;
+
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
