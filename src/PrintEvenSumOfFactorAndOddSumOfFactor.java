617543b1b60d87ca925ba0869c0203deea1bd6c4
karthikeyan.V <karthikarthi85544@gmail.com>
2024-06-04 22:37:50 +0530

    Print even sum and odd sum

--- src/PrintEvenSumOfFactorAndOddSumOfFactor.java
+++ src/PrintEvenSumOfFactorAndOddSumOfFactor.java
@@ -1,17 +1,16 @@
 package src;
 import java.util.Scanner;
-public class PrintEvensumofFactorAndOddsumofFactor {
+public class PrintEvenSumOfFactorAndOddSumOfFactor {
+
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = sc.nextInt();
 
         int evenSum = 0;
         int oddSum = 0;
 
-
-        for( ;num > 0; num = num / 10) {
+        for( int num = sc.nextInt(); num > 0; num = num / 10) {
             int reminder = num % 10;
 
             if(reminder %2 == 0) {
@@ -22,9 +21,6 @@ public class PrintEvensumofFactorAndOddsumofFactor {
         }
         System.out.println("Even number is " + evenSum);
         System.out.println("odd number is " + oddSum);
-
-
-
     }
 
 }
