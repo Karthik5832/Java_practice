bc5269ea7c9bef7314e019e80b33cdbb1e7df472
karthikeyan.V <karthikarthi85544@gmail.com>
2024-06-05 00:41:47 +0530

    Sum of all digits like Enter the number: 2999 29 11 2

--- src/SumOfAllDigits.java
+++ src/SumOfAllDigits.java
@@ -2,26 +2,26 @@ package src;
 import java.util.Scanner;
 
 public class SumOfAllDigits {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int n = sc.nextInt();
+        int num = sc.nextInt();
         int sum = 0;
 
-        while (n > 0) {
-            int i = (n % 10);
-            sum = sum + i;
-            n = n / 10;
+        while (num > 0) {
+            int reminder = num % 10;
+            sum = sum + reminder;
+            num = num / 10;
 
-            if(n==0 && sum>9) {
+            if(num == 0 && sum > 9) {
                 System.out.println(sum);
-                n = sum;
+                num = sum;
                 sum = 0;
             }
         }
 
-        System.out.println(sum);
+       System.out.println(sum);
     }
 }
 
