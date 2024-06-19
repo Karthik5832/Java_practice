a574a8aadca219d47763121deb8b9ac4427960d5
karthikeyan.V <karthikarthi85544@gmail.com>
2024-06-19 21:45:48 +0530

    To print the entered choice 1. Sum of all  2. Prime using method calling

--- src/SumOfAllDigits.java
+++ src/SumOfAllDigits.java
@@ -4,11 +4,33 @@ import java.util.Scanner;
 public class SumOfAllDigits {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int num = sc.nextInt();
-        int sum = 0;
 
+        System.out.println("1. Sum of all  2. Prime");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Choice: ");
+        int choice = sc.nextInt();
+        if(choice == 1) {
+            System.out.print("Enter the number: ");
+            int num = sc.nextInt();
+            int sum = sumOffAllDigits(num);
+            System.out.println(sum);
+        } else if(choice == 2) {
+            System.out.println("Enter two numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            displayPrimeNumbers(a, b);
+        } else {
+            System.out.println("Invalid choice.");
+        }
+    }
+
+
+    static void displayPrimeNumbers(int a, int b) {
+
+    }
+
+    static int sumOffAllDigits(int num) {
+        int sum = 0;
         while (num > 0) {
             int reminder = num % 10;
             sum = sum + reminder;
@@ -20,8 +42,8 @@ public class SumOfAllDigits {
                 sum = 0;
             }
         }
+        return sum;
 
-       System.out.println(sum);
     }
 }
 
