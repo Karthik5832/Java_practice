d43d3f09c959dd1e67df747ea11bd7ee00c42c42
karthikeyan.V <karthikarthi85544@gmail.com>
2024-06-19 21:43:42 +0530

    To print k structure using nested loop

--- src/Pattern4UsingNestedLoop.java
+++ src/Pattern4UsingNestedLoop.java
@@ -1,21 +1,23 @@
 package src;
 import java.util.Scanner;
+
 public class Pattern4UsingNestedLoop {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
 
-        for(int i = 1; i<=num; i++){
-            for(int j = 1; j<=num; j++) {
-                if(j == 1 ){
-                    System.out.println("X");
-                    for(int k = 1;k <= num;k++){
+        for (int i = 0; i < num; i++) {
+            for (int j = 1; j <= num; j++) {
 
-                    }
+                if (j == 1 || i == j || i + j == num -1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
+            System.out.println();
         }
-
     }
 }
